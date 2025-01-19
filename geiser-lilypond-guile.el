3e146a25a066dba330489bd674459e6be802addd
Saul James Tobin <saul.james.tobin@gmail.com>
2025-01-19 12:40:22 -0500

    Add Geiser Lilypond implementation
    Use Lilypond binary as REPL for Geiser, with autodoc support for signatures
    of primitive functions exported from Lilypond C++ codebase.

---
+++ geiser-lilypond-guile.el
@@ -0,0 +1,41 @@
+;;; geiser-lilypond-guile.el --- Lilypond Geiser REPL -*- lexical-binding: t -*-
+
+;; Copyright (c) 2025 Saul James Tobin
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+(use-package geiser-guile)
+
+(defvar ly-guile-bin "lilypond")
+(defvar ly-guile-args '("scheme-sandbox"))
+(defvar ly-guile-version "3.0.10")
+
+(defvar ly-guile-dir
+  (file-name-directory (or load-file-name buffer-file-name)))
+(add-to-list 'geiser-guile-load-path (file-name-concat ly-guile-dir "scm/"))
+
+(defun ly-guile-repl-startup (address)
+  (geiser-guile--startup (or address t))
+  (geiser-eval--send/wait "(load-from-path \"ly-guile-autodoc.scm\")"))
+
+(define-geiser-implementation (lilypond-guile guile)
+                              (binary ly-guile-bin)
+                              (arglist ly-guile-args)
+                              (repl-startup ly-guile-repl-startup)
+                              (version-command ly-guile-version))
+(geiser-implementation-extension 'lilypond-guile "scm")
+(geiser-activate-implementation 'lilypond-guile)
+
+(provide 'geiser-lilypond-guile)
+;;; geiser-lilypond-guile.el ends here

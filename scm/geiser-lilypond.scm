2a57dd4a423811adc6b0676e0ac4ed9b0a46c806
Saul James Tobin <saul.james.tobin@gmail.com>
2025-01-28 16:35:45 -0500

    Cleanup: name prefixes, scm exports

--- scm/geiser-lilypond.scm
+++ scm/geiser-lilypond.scm
@@ -21,7 +21,9 @@
             ly:all-translator-names
             ly:list-builtin-constants
             ly:music-word?
-            keywords-of-type)
+            keywords-of-type
+            ly:grob-property-completions
+            ly:accepts-maybe-property-path?)
   #:use-module (ice-9 and-let-star)
   #:use-module (ice-9 regex)
   #:use-module (ice-9 session)

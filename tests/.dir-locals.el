1c22981c95a5020a2c4d77d34cd051d2e1aa52a3
Saul James Tobin <saul.james.tobin@gmail.com>
2025-03-01 14:14:02 -0500

    Moment navigation: update demo for new implementation

--- tests/.dir-locals.el
+++
@@ -1,2 +0,0 @@
-((nil . ((lilypond-ts-moment-eval-config . ((("c:/Users/saulj/AppData/Roaming/.emacs.d/lilypond-ts-mode/tests/moment-navigation.ly"
-                                              "c:/Users/saulj/AppData/Roaming/.emacs.d/lilypond-ts-mode/tests/moment-navigation-2.ily")))))))

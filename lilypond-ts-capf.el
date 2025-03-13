db3f477f73df08b66d0073b9d917792fb2fabea1
Saul James Tobin <saul.james.tobin@gmail.com>
2025-03-13 01:22:34 -0400

    Capf: function documentation

--- lilypond-ts-capf.el
+++ lilypond-ts-capf.el
@@ -105,9 +105,80 @@
          capf-rules)))
 
 (defun lilypond-ts--treesit-capf ()
+  "Generate completions at point by applying `lilypond-ts--treesit-capf-rules',
+
+The concept is to support smart contextual narrowing of completion suggestions
+via a two-step matching algorithm.
+
+Consider the example of a LilyPond property expression: `symbol.symbol'. There
+are various possible sets of words that could fit in either node, but depending
+on the text of the other symbol, only certain sets make sense. For example, if
+the first symbol is a grob type, it makes no sense for the second to be a
+context property. There are further logical dependencies when additional
+surrounding nodes are considered.
+
+This situation could be handled via a `treesit' query using :match or :pred, but
+consider that in this example there are multiple mutually exclusive sets of
+possible valid completion sets for the nodes in question. To handle this just
+with queries would mean constructing a multitude of queries differing only in
+their filtering predicate clauses, and during matching running the parse tree
+search over and over only to discard the results whenever a predicate failed to
+match. A better design is to run queries purely to capture the configuration of
+the parse tree, then iteratively try matching each set of constraints against
+the captured nodes to find the valid completions. Also notice that the
+constraints are agnostic to which node it is that needs to be matched.
+
+Accordingly, each rule consists of a query predicate accepting the node at point
+and returning an alist of captured nodes and a list of \"masks\", each
+representing one consistent set of completions for one or more nodes captured by
+the query predicate. To start with, imagine that each element of a mask is just
+list of words in a semantic category, which could be used either to match a
+neighboring node's text or as completion suggestions.
+
+It probably only makes sense for the masks to have the same number of elements
+as the query's capture groups, but we need to handle the situation where that
+doesn't occur, for instance if a query captures optional or quantified nodes.
+Also, `treesit' doesn't group nodes captured by the same instance of the query,
+so we need to handle the situation where the query predicate returns nodes from
+capture groups that aren't even in the right neighborhood.
+
+To do this, we iterate over the cons cells of the capture list, seeing if the
+first n nodes match against each mask. You can picture this as \"sliding\" the
+mask along the list of captured nodes until it slots in where each element of
+the mask lines up with a matching node. If that happens, we retrieve the
+completions from whichever element of the mask lined up with the node at point,
+add them to the completion table, and go on to check if any other masks match.
+
+In reality, mask elements are not simple word lists, but pairs of functions: a
+predicate used to check whether the text of a node matches, and a function that
+accepts the text of each other node matched by the mask, returning a completion
+table. By default, these functions are set up to encode via lexical closures
+equivalent behavior to a simple word list, but this design allows for specific
+completion categories to use more complicated contextual logic. In the case of
+`lilypond-ts-mode', the text of the grob type node is used to only suggest grob
+properties that are valid for that specific grob.
+
+A few last details:
+
+The query predicate is technically not run on the node at point, but the node
+spanning the character before point, in order to properly handle completion at
+node boundaries.
+
+While all matching masks belonging to the same rule will supply completions,
+rules are run in order and completions from only one rule will be returned.
+
+Rules may, in addition to a query predicate and a list of masks, specify a
+post-process function. This is applied to the final combined completion table,
+and is useful for instance with `completion-table-subvert'.
+
+Completion metadata from `lilypond-ts--capf-properties' is appended to the capf
+return value.
+
+Note that the data structures described here are the compiled forms. Users
+should generally not directly write mask lambdas."
   (cl-loop
    with node = (treesit-node-on (1- (point)) (point))
-   for (query-pred masks filter) in lilypond-ts--treesit-capf-rules
+   for (query-pred masks post-process) in lilypond-ts--treesit-capf-rules
    for captures = (funcall query-pred node)
    when captures
    thereis (cl-loop
@@ -129,7 +200,7 @@
             when words collect words into tables
             finally return `(,(treesit-node-start node)
                              ,(treesit-node-end node)
-                             ,(funcall (or filter #'identity)
+                             ,(funcall (or post-process #'identity)
                                        (apply #'completion-table-merge
                                               tables))
                              . ,lilypond-ts--capf-properties))))

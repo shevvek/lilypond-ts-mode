d9239c1b41bb02f37b2c87133e870be29bef4604
Saul James Tobin <saul.james.tobin@gmail.com>
2025-03-26 04:17:05 -0400

    Tests: convenience function to run tests in batch mode

---
+++ tests/lilypond-ts-test-batch.el
@@ -0,0 +1,43 @@
+;;; lilypond-ts-test-batch.el --- Regression tests -*- lexical-binding: t -*-
+
+;; Copyright (c) 2025 Saul James Tobin
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with lilypond-ts-mode.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Convenience function to batch run regression tests.
+
+;;; Code:
+
+(require 'lilypond-ts-base)
+(require 'cl-lib)
+
+(defun lilypond-ts-test--run-batch-tests ()
+  (interactive)
+  (start-process "emacs - lilypond-ts-mode batch tests" "*LilyPond TS Tests*"
+                 (expand-file-name invocation-name invocation-directory)
+                 "-Q" "--batch"
+                 "-L" (cl-find "geiser[^a-zA-Z]+$" load-path
+                               :test #'string-match-p)
+                 "-L" (cl-find "geiser-guile[^a-zA-Z]+$" load-path
+                               :test #'string-match-p)
+                 "-L" (expand-file-name lilypond-ts-location)
+                 "-L" (expand-file-name "tests/" lilypond-ts-location)
+                 "-l" "lilypond-ts-tests"
+                 "-l" "lilypond-ts-nav-tests"
+                 "-f" "ert-run-tests-batch-and-exit"))
+
+(provide 'lilypond-ts-test-batch)
+;;; lilypond-ts-test-batch.el ends here
